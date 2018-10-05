bc09e469405c5d6b1831edd21efdd766c9371995
Peliukas <pavel.kocia@gmail.com>
2018-10-05 11:24:46 +0200

    Created basic page layout;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { MatCardModule, MatButtonModule, MatDividerModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MatCardModule,MatButtonModule, MatDividerModule,
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
